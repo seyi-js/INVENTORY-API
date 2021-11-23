0f85f191acf09e1ef1e2a4051c4504a5ef4a9364
SAMUEL ADEBAYO OLUWASEYI <adebayosamueljahsmine30@gmail.com>
2021-11-23 01:47:52 +0100

    .

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-const PORT =   80
+const PORT =  process.env.PORT || 3000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({transform:true}));
